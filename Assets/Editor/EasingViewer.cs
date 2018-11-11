e3dd2479845322d53edf5c7249b78c13d9814d80
Syy <shirakawa.yy@gmail.com>
2018-11-11 22:42:00 +0900

    Add empty EditorWindow

---
+++ Assets/Editor/EasingViewer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Syy.Easing
+{
+    public class EasingViewer : EditorWindow
+    {
+        [MenuItem("Tools/EasingViewer")]
+        public static void Open()
+        {
+            GetWindow<EasingViewer>();
+        }
+    }
+}
